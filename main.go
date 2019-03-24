5b43a0b7a6ce6e46e38cd5563b51ec3397db7db9
ZzqiZQute <zhaozq11th@ustbsmartcar.org>
2019-03-24 21:09:35 +0800

    fix bugs

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"log"
 	"fmt"
+	"sync"
 )
 
 func handleErr(err error){
@@ -16,24 +17,28 @@ func main() {
 	var cnt int
 	count:=0
 	count_recv:=0
-	addr, err := net.ResolveTCPAddr("tcp4", "localhost:8899")
+	lock:=sync.Mutex{}
+	addr, err := net.ResolveTCPAddr("tcp4", "192.168.1.100:8899")
 	handleErr(err)
 	receiver, err := net.DialTCP("tcp4", nil, addr)
 	handleErr(err)
 	fmt.Println("Connect to server,start dispatching on port 8900!")
 	buffer:=make([]byte,1024)
-	ch:=make(chan struct{})
-	ch2:=make(chan struct{})
-	ch3:=make(chan struct{})
+	ch1 :=make(chan struct{})
+	ch2 :=make(chan struct{})
 	go func() {
 		for {
-			<-ch
+			lock.Lock()
 			count=0
 			cnt, err = receiver.Read(buffer)
-			for i:=0;i<count_recv;i++{
-				ch3<- struct{}{}
-			}
 			handleErr(err)
+			if count_recv !=0 {
+				for i := 0; i < count_recv; i++ {
+					ch2 <- struct{}{}
+				}
+			}else{
+				lock.Unlock()
+			}
 		}
 	}()
 	go func() {
@@ -47,19 +52,18 @@ func main() {
 			handleErr(err)
 			go func() {
 				for {
-					<-ch3
+					<-ch2
 					b := make([]byte, cnt)
 					copy(b, buffer)
 					listener.Write(b)
 					count++
 					if count >= count_recv {
-						ch <- struct{}{}
+						lock.Unlock()
 					}
 				}
 			}()
 		}
 	}()
-	ch<- struct{}{}
-	<-ch2
+	<-ch1
 }
 
